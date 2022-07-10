34a896917ebcd0337a7ffefcf9318ebed7f5f1be
Karim Darwish <darwish_karim@outlook.com>
2022-07-10 17:39:41 +0200

    feat: add health check

--- src/PaymentGateway.API/Program.cs
+++ src/PaymentGateway.API/Program.cs
@@ -4,10 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocumentation();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health");
 
 if (app.Environment.IsDevelopment())
 {
