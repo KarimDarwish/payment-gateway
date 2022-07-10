9b56e34973f41ff36cc81473279ba178e9990d87
Karim Darwish <darwish_karim@outlook.com>
2022-07-10 17:16:39 +0200

    Add Swagger (#6)
    * feat: add swagger
    
    * ci: publish swagger to github pages
    
    * fix: build command
    
    * feat: add badge to readme
    
    * ci: adapt swagger step to only run on push to main

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Payment Gateway
+![](https://validator.swagger.io/validator?url=https://karimdarwish.github.io/checkout-payment-gateway/swagger.json)
---
+++ src/PaymentGateway.API/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace PaymentGateway.API.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Payment Gateway",
+                Description =
+                    "A payment gateway that allows merchants to offer their shoppers a secure way to pay for their products",
+            });
+        });
+    }
+}
