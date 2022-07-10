80e01bb65b25f5b9ec540fa8ca3baef5c18da9e5
Karim Darwish <darwish_karim@outlook.com>
2022-07-10 16:51:50 +0200

    feat: add swagger

--- src/PaymentGateway.API/Program.cs
+++ src/PaymentGateway.API/Program.cs
@@ -1,6 +1,18 @@
+using PaymentGateway.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocumentation();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
