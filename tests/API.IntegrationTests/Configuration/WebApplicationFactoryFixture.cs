14c49f88f1116ea99051d28f28c0389a183bcefd
Karim Darwish <darwish_karim@outlook.com>
2022-07-10 16:09:39 +0200

    feat(test): add integration test setup with base class

---
+++ tests/API.IntegrationTests/Configuration/WebApplicationFactoryFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace API.IntegrationTests.Configuration;
+
+public class WebApplicationFactoryFixture : IDisposable
+{
+    internal WebApplicationFactory<Program> Factory { get; }
+
+    public WebApplicationFactoryFixture()
+    {
+        Factory = new WebApplicationFactory<Program>();
+    }
+
+    public void Dispose()
+    {
+        Factory.Dispose();
+    }
+}
--- tests/API.IntegrationTests/UnitTest1.cs
+++ tests/API.IntegrationTests/UnitTest1.cs
@@ -1,11 +1,25 @@
+using System.Threading.Tasks;
+using API.IntegrationTests.Configuration;
+using FluentAssertions;
 using Xunit;
 
 namespace API.IntegrationTests;
 
-public class UnitTest1
+public class UnitTest1 : IntegrationTest
 {
-    [Fact]
-    public void Test1()
+    public UnitTest1(WebApplicationFactoryFixture fixture) : base(fixture)
     {
+    }
+
+    [Fact]
+    public async Task DefaultEndpoint_returnsCorrectResponse()
+    {
+        //Act
+        var response = await Client.GetAsync("/");
+
+        //Assert
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        responseString.Should().Contain("Hello World");
     }
 }
