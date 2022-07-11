56bb1e94b00bca67ad9d29eb88ff48eea9dcc403
Karim Darwish <darwish_karim@outlook.com>
2022-07-11 13:02:13 +0200

    Add Payment Repository (#14)
    feat: add payment repository

---
+++ src/PaymentGateway.Domain/Repositories/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using PaymentGateway.Domain.Entities;
+
+namespace PaymentGateway.Domain.Repositories;
+
+public interface IPaymentRepository
+{
+    void Insert(Payment payment);
+    Payment? Get(Guid paymentId);
+}
---
+++ src/PaymentGateway.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+using PaymentGateway.Domain.Entities;
+using PaymentGateway.Domain.Exceptions;
+using PaymentGateway.Domain.Repositories;
+
+namespace PaymentGateway.Infrastructure.Repositories;
+
+public class PaymentRepository : IPaymentRepository
+{
+    //Using a dictionary over a list due to computational complexity
+    //The ConcurrentDictionary can deal with concurrent updates to the data set (simulating database locking mechanisms)
+    private readonly IDictionary<Guid, Payment> _payments = new ConcurrentDictionary<Guid, Payment>();
+
+    public void Insert(Payment payment)
+    {
+        try
+        {
+            _payments.Add(payment.Id, payment);
+        }
+        catch (ArgumentException)
+        {
+            throw new PaymentAlreadyExistsException();
+        }
+    }
+
+    public Payment? Get(Guid paymentId)
+    {
+        try
+        {
+            return _payments[paymentId];
+        }
+        catch (KeyNotFoundException)
+        {
+            return null;
+        }
+    }
+}
