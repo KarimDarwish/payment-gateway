d49d83e298f37e17693a3503294fa5240db54d26
Karim Darwish <darwish_karim@outlook.com>
2022-07-11 11:00:16 +0200

    feat: add card expiry and cvv value objects

---
+++ src/PaymentGateway.Domain/ValueObjects/CardExpiry.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace PaymentGateway.Domain.ValueObjects;
+
+public class CardExpiry
+{
+    public int Month { get; }
+    public int Year { get; }
+    public bool HasExpired { get; }
+
+    public CardExpiry(int month, int twoDigitYear)
+    {
+        if (month is > 12 or < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(month), month,
+                "Provided month is not within the allowed range of 1 - 12");
+        }
+
+        Month = month;
+        Year = twoDigitYear;
+        HasExpired = HasCreditCardExpired(Month, Year);
+    }
+
+    private static bool HasCreditCardExpired(int month, int year)
+    {
+        var dateOfExpiry = DateTimeOffset.ParseExact($"{month}/{year}", "MM/yy", CultureInfo.InvariantCulture);
+
+        return dateOfExpiry < DateTimeOffset.UtcNow;
+    }
+}
---
+++ tests/Domain.UnitTests/ValueObjects/CardExpiryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using PaymentGateway.Domain.ValueObjects;
+using Xunit;
+
+namespace Domain.UnitTests.ValueObjects;
+
+public class CardExpiryTests
+{
+    [Fact]
+    public void Create_withInvalidMonth_throws()
+    {
+        //Arrange
+        var invalidMonth = 13;
+
+        //Act
+        var action = () => new CardExpiry(invalidMonth, 25);
+
+        //Assert
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void Create_withExpiredCard_isExpired()
+    {
+        //Arrange
+        var expiredCardYear = 20;
+
+        //Act
+        var cardExpiry = new CardExpiry(12, expiredCardYear);
+
+        //Assert
+        cardExpiry.HasExpired.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Create_withValidCard_isNotExpired()
+    {
+        //Act
+        var cardExpiry = new CardExpiry(12, 25);
+
+        //Assert
+        cardExpiry.HasExpired.Should().BeFalse();
+    }
+}
